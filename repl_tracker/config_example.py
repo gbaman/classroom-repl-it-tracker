b6abac186e8c3b54f6dfe47df6b8252517a44aaf
Andrew Mulholland <gbaman@users.noreply.github.com>
2023-11-30 10:20:36 +0000

    Update config example to newer teams format

--- repl_tracker/config_example.py
+++ repl_tracker/config_example.py
@@ -3,13 +3,13 @@
 from config_helper import YearGroup
 
 # Example below of how to define a "year group".
-# YearGroup("Year 10",      123456,           [123456, 123457, 123458, 123459],          ['1.0', '1.1', '1.2', '5.2'])
-#           Name       Master classroom ID  All classroom IDs in this year group   Required exercises (using 1.1 etc format)
+# YearGroup("Year 10",      123456,                         ["great-team", "hello_world_team"],          ['1.0', '1.1', '1.2', '5.2'])
+#           Name       A unique ID number (make one up)      All Team names (check team URL)          Optional required (using 1.1 etc format)
 # The minimum required example below would be
-# years = [YearGroup("YEAR-NAME", 123456, [123456], [])]
+# years = [YearGroup("YEAR-NAME", 123456, ["replit-team-name"], [])]
 
 # System supports as many year groups as required.
-# You can get the classroom ID from the URL on repl classroom.
+# You can get the team ID from the URL on repl classroom.
 
 years = [YearGroup("YEAR-NAME", 123456, [123456], []),
 
@@ -21,7 +21,8 @@ ignored_usernames = []
 # If including teacher initials in the Team name, add any teachers in the list below (used for emails)
 teacher_initials = []
 
-# Used to create subgroups in a single Team. Format should be email_address, class_name
+# Optional - Used to create subgroups in a single Team. Format should be email_address, class_name in a CSV file.
+# Must be a web URL to the CSV file (for example, stored in OneDrive or Google Drive etc)
 student_csv_file_path = ""
 
 # Used for email reminders for students
